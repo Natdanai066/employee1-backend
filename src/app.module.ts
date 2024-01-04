6d1d1f28a4f87ffc73d6ddd101ec44476f188de5
Natdanai066 <s63122201066@ssru.ac.th>
2024-01-05 03:13:11 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeeModule } from './employee/employee.module';
 import { Employee } from './employee/entities/employee.entity';
 
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -12,7 +13,7 @@ import { Employee } from './employee/entities/employee.entity';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '',
+      password: "",
       database: 'employeesystem',
       entities: [Employee],
       synchronize: true,
