635b360cb00cdb6eaba7c5a5b9ccd8d2f82adc5d
Natdanai066 <s63122201066@ssru.ac.th>
2024-01-06 21:42:30 +0700

    new back

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,11 @@ import { Employee } from './employee/entities/employee.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
+      host: 'viaduct.proxy.rlwy.net',
+      port: 24877,
       username: 'root',
-      password: "",
-      database: 'employeesystem',
+      password: "gbg452dDBEdBB5-C-Bb331DBaA62d563",
+      database: 'railway',
       entities: [Employee],
       synchronize: true,
     }),
